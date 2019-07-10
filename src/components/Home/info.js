4cda224b17f6fdd1edabe1134ce8141f8d33798f
ZPPD <zhana.p.d@gmail.com>
2019-07-10 14:39:03 -0400

    background, about page, footer

---
+++ src/components/Home/info.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "gatsby"
+import Title from "../Globals/Title"
+
+export default function info() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <Title title="our story" />
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              We work side by side with farmers around the world to strengthen
+              farming practices to cultivate the best coffee beans in the world.
+              Then, we train our baristas in the sacred art of the brew process,
+              yielding the most flavorful coffee you've ever tasted.
+            </p>
+            <Link to="/about/">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
